f3682249b5d60f8607fc624e38a3e69e445dc380
Chatura Kuruppu <ckuruppu@email.sc.edu>
2019-11-21 14:46:37 -0600

    upload.sh was added. Not really a necessary file to generate histograms, but required to transfer files between local and the novagpvm.

---
+++ cpp/createHistograms/upload.sh
@@ -0,0 +1,51 @@
+#! /bin/sh
+
+COPYFILE_DISABLE=1; 
+export COPYFILE_DISABLE
+
+declare -a fileList=("make.sh"
+                     "upload.sh" 
+                     "makeHistograms.cxx"
+                     "makeHistograms.h" 
+                     "headers.h" 
+                     "structs.h" 
+                     "createHistograms.cxx")
+echo "\n"
+echo "Uploading following files: "
+for i in "${fileList[@]}"
+do
+echo "$i"
+done
+echo "\n"
+echo "Generating upload.tar.gz file: "
+for i in "${fileList[@]}"
+do
+tar rf upload.tar "$i"
+done
+gzip upload.tar
+echo "Done..!"
+
+echo "\nChecking the contents inside upload.tar: "
+tar -tvf upload.tar.gz
+
+echo "\nUploading upload.tar.gz: "
+scp ./upload.tar.gz ckuruppu@novagpvm15.fnal.gov:/nova/app/users/ckuruppu/workingThesis/cpp/createHistograms/
+
+echo "\nssh into novagpvm and clean build: "
+ssh ckuruppu@novagpvm15.fnal.gov "cd /nova/app/users/ckuruppu/workingThesis/cpp/createHistograms/ &&
+                                  tar xopf upload.tar.gz &&
+                                  rm upload.tar.gz &&
+                                  echo 'upload.tar.gz was deleted successfully..!' 
+                                  "
+
+rm upload.tar.gz
+
+
+
+#tar -zcf upload.tar --exclude='.*' makeHistograms.cxx makeHistograms.h headers.h structs.h makeHistograms.h createHistograms.cxx
+# scp ./upload.tar ckuruppu@novagpvm15.fnal.gov:/nova/app/users/ckuruppu/workingThesis/cpp/createHistograms/
+# ssh ckuruppu@novagpvm15.fnal.gov "cd /nova/app/users/ckuruppu/workingThesis/cpp/createHistograms/ &&
+#                                   rm makeHistograms.cxx makeHistograms.h headers.h structs.h makeHistograms.h createHistograms.cxx &&
+#                                   echo 'Old files were cleaned..!' &&
+#                                   tar xopf upload.tar
+#                                   "
